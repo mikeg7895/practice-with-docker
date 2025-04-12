514376fc051ba1dde375c3589230eb2dfd06da0e
mikeg7895 <maykollgomez050@gmail.com>
2025-04-12 13:34:24 -0500

    url changed

--- frontend/tasks/src/App.jsx
+++ frontend/tasks/src/App.jsx
@@ -2,7 +2,7 @@
 import { useEffect, useState } from 'react';
 import axios from 'axios';
 
-const API_URL = 'http://localhost:8000/tasks';
+const API_URL = 'http://3.138.125.162/tasks';
 
 function App() {
   const [tasks, setTasks] = useState([]);
